8608d742e18e63594b190f9d73fe8c59f2ac0587
HoangViet <hoangphamhl000@gmail.com>
2024-06-11 21:31:29 +0700

    Build contact information interface

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { text } from '../utils/dataContact'
+import { Buttons } from '../components'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div className='border-[7px] border-dashed border-blue-100 bg-white rounded-md shadow-md p-4 w-3/5 flex flex-col justify-center items-center gap-6 mt-4'>
+            <img className='w-full h-48 object-contain' src={text.image} alt="thumbnail" />
+            <p className='font-arial'>{text.content}</p>
+            <div className='flex items-center justify-around w-full'>
+                {text.contact.map((item, index) => {
+                    return (
+                        <div key={index} className='flex flex-col items-center justify-center font-arial'>
+                            <span className='text-orange-600 text-sm font-semibold'>{item.text}</span>
+                            <span className='text-blue-900 text-lg font-semibold'>{item.phone}</span>
+                            <span className='text-blue-900 text-lg font-semibold'>{item.zalo}</span>
+                        </div>
+                    )
+                })}
+            </div>
+            <Buttons
+                text='Gửi liên hệ'
+                bgColor='bg-[#3961fb]'
+                textColor='text-white'
+                px='px-7'
+                fontWeight='font-bold'
+            />
+        </div>
     )
 }
 
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { text } from '../utils/dataIntro'
 import icons from '../utils/icons'
 import { Buttons } from '../components'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { formatVietnameseToString } from '../utils/Common/formatVietNameseToString'
 
 const { GrStar } = icons
 const stars = [1, 2, 3, 4, 5]
 
 const Intro = () => {
+
+    const { categories } = useSelector(state => state.app)
+
     return (
         <div className='border w-3/5 bg-white rounded-md shadow-md p-4 gap-4 flex flex-col justify-center items-center'>
             <h3 className='font-semibold text-lg font-arial'>{text.title}</h3>
-            <p className='text-gray-800 text-center font-arial my-4 text-sm'>{text.description}</p>
+            <p className='text-gray-800 text-center font-arial my-4 text-sm'>
+                {text.description}
+                <span className='text-link'>
+                    {categories?.length > 0 && categories.map(item => {
+                        return (
+                            <Link
+                                key={item.code}
+                                to={`/${formatVietnameseToString(item.value)}`}
+                                className='text-blue-700  font-arial font-semibold hover:text-orange-600'
+                            >
+                                {`${item.value.toLowerCase()}, `}
+                            </Link>
+                        )
+                    })}
+                </span>
+                {text.description2}
+            </p>
             <div className='flex items-center justify-around w-full font-arial'>
                 {text.statistics.map((item, index) => {
                     return (
@@ -39,10 +61,12 @@ const Intro = () => {
                 text='Đăng tin ngay'
                 bgColor='bg-secondary2'
                 textColor='text-white'
+                px='px-6'
+                fontWeight='font-bold'
             />
             <div className='h-[50px]'></div>
         </div>
     )
 }
 
-export default Intro
+export default memo(Intro)
